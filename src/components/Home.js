8a8711d92f15e9e0349fd4021363076761c93c85
colin <colinnoahpeter@gmail.com>
2019-08-20 18:40:11 +0800

    improved carousell images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,123 @@
-import React from 'react';
-import {Card, Row, Col, UncontrolledCarousel } from 'reactstrap';
+import React, { Component } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 import Community from "../images/community.jpg"
 import Healthy from "../images/Healthy.jpg"
 import Fitness from "../images/Fitness.jpg"
 
+
+
 const items = [
     {
+        id:1,
         src: Community,
         altText: '',
-        caption: '',
+        caption: 'Meet New Fitness Partners',
         header: ''
     },
     {
+        id:2,
         src: Healthy,
         altText: '',
-        caption: '',
+        caption: 'Organize meet-ups at your favourite spots - anytime, anywhere',
         header: '',
 
     },
     {
+        id:3,
         src: Fitness,
         altText: '',
-        caption: '',
+        caption: 'Have enough players - everytime',
         header: ''
     }
 ];
 
-const Home = (props) => {
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
 
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+    console.log(items)
+    const slides = items.map((item) => {
+        console.log(item)
+        console.log(item.id)
+        console.log(item.src)
+      return (
+        <CarouselItem
+          className="custom-tag"
+          tag="div"
+          key={item.id}
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+        >
+        <img src={item.src} alt={item.altText} style={{maxWidth:'100%', transform:'translateY(-30%)'}}/>
+          <CarouselCaption className="text-light" captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
 
     return (
-        <>  
-        <UncontrolledCarousel items={items}/>
-            <Row>
-                <Col sm="12">
-                    <Card body>
-                        <h1 align="center">Community bonding with Sports</h1>
-                        <h2 align="center">Trying to look for random people to do sports? <br></br>We are here for loners who wants to do some outdoor activities with other lonely people</h2>
-                    </Card>
-                </Col>
-            </Row>
-        </>
+      <div>
+        <style>
+          {
+            `.custom-tag {
+                max-width: 100%;
+                height: 600px;
+                background: black;
+              }`
+          }
+        </style>
+        <Carousel
+          activeIndex={activeIndex}
+          next={this.next}
+          previous={this.previous}
+        >
+          <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+          {slides}
+          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+          <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+        </Carousel>
+      </div>
     );
+  }
 }
 
 export default Home;
